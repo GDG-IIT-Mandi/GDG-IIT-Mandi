bd4c7593b31561826d83e7f2427da2cefc14d370
Vinamra Garg <gargvinamra21@gmail.com>
2024-10-30 11:01:06 +0530

    path correction

---
+++ app/About/page.tsx
@@ -0,0 +1,9 @@
+import GDGTeam from '../components/GDGTeam';
+
+export default function ContactUs() {
+  return (
+      <main>
+        <GDGTeam />
+      </main>
+  );
+}
--- app/TeamGDG/page.tsx
+++ app/TeamGDG/page.tsx
@@ -1,6 +1,4 @@
 import GDGTeam from '../components/GDGTeam';
-import SAETeam from '../components/GDGTeam';
-
 export default function ContactUs() {
   return (
       <main>
